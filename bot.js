f915b7df9259f5912f473e8516bdec26bb87e365
Elow <kevin.basin@gmail.com>
2017-12-08 11:43:00 +0100

    Change keepalive every 5 min

--- bot.js
+++ bot.js
@@ -434,7 +434,7 @@ for (var i = breaks.length - 1; i >= 0; i--) {
     });
 }
 // Keepalive
-schedule.scheduleJob("0 59 * * * *", function() {
+schedule.scheduleJob("0 /5 * * * *", function() {
     request(process.env.APP_URL, function (error, response, body) {
       console.log('error:', error); // Print the error if one occurred
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
